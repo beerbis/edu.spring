b98ba5ab22b759baac495567b99e71e1d2fa61a7
Виктор Елсуков <beerbis@yandex.ru>
2021-05-03 01:58:55 +0300

    chore: инициализировтаь скелет проекта "прима SpringDataJpa" со start.spring.io

---
+++ datajpaexmple/src/test/java/ru/beerbis/datajpaexample/DataJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.beerbis.datajpaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
